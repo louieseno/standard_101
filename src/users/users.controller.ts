1230d75e1d78b492cd1aa7cedab043e2866e70eb
Louie Seno <senolouiejohn.dev@gmail.com>
2024-12-09 04:07:47 +0800

    Setup users controller request handler

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role?: 'ADMIN' | 'EMPLOYEE'): [] {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): object {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): object {
+    return { id };
+  }
+}
