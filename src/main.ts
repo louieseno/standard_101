e6547efada1340bd50e640a9be279572dd4d79c8
Louie Seno <senolouiejohn.dev@gmail.com>
2024-12-17 04:35:54 +0800

    Add Swagger definition

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1')
+
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('Basic REST API for Users.')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,36 @@
-import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Param, ParseIntPipe, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User, UserRole } from './entities/user.entity';
 import { UpdateUserEntity } from './entities/update_user.entity';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Get()
+  @ApiQuery({ name: 'role', enum: UserRole, required: false })
+  @ApiOperation({
+    summary: "Fetch list of users"
+  })
   findAll(@Query('role') role?: UserRole): Promise<User[]> {
     return this.usersService.findAll(role);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: "Find user by ID" })
   async findOne(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number): Promise<User> {
     return this.usersService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: "Create a user" })
   create(@Body(ValidationPipe) user: User): Promise<User> {
     return this.usersService.create(user);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: "Update a user property/properties" })
   update(
     @Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number,
     @Body(ValidationPipe) user: UpdateUserEntity,
@@ -31,6 +39,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: "Hard delete a user" })
   delete(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number): Promise<User> {
     return this.usersService.delete(id)
   }
