50cd0e0397d06b2c3d4f1bb65bceea1dfca10436
Louie Seno <senolouiejohn.dev@gmail.com>
2024-12-29 10:38:47 +0800

    Add unit test for users

---
+++ src/users/test/users.service.spec.ts
@@ -0,0 +1,111 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users.service';
+import { User, UserRole } from '../entities/user.entity';
+import { UpdateUserEntity } from '../entities/update_user.entity';
+import { UsersController } from '../users.controller';
+import { NotFoundException } from '@nestjs/common';
+
+const mockId = 1;
+const mockUser: User = {
+  "id": mockId,
+  "name": "jonathan kuminga",
+  "email": "jonathan@example.com",
+  "role": UserRole.EMPLOYEE,
+  "created_at": new Date(),
+  "update_at": new Date(),
+}
+
+const mockPartialUser: UpdateUserEntity = {
+  "name": "andrew kuminga",
+  "email": "andrew@example.com",
+  "update_at": new Date(),
+}
+
+const mockAllUser = { data: [] };
+const mockIdError = 'error';
+class MockedUsersModel {
+  constructor(private _: any) { }
+  new = jest.fn().mockResolvedValue({});
+  static findAll = jest.fn().mockReturnValue([mockUser]);
+  static findOne = jest.fn().mockReturnValue(mockUser);
+  static create = jest.fn().mockReturnValue(mockUser);
+  static update = jest.fn().mockReturnValue({ ...mockUser, ...mockPartialUser });
+  static delete = jest.fn().mockReturnValue(mockUser);
+
+  // static save = jest.fn().mockResolvedValue(mockUser);
+  // static find = jest.fn().mockReturnThis();
+  // static create = jest.fn().mockReturnValue(mockUser);
+  // static findOneAndDelete = jest.fn().mockImplementation((id: string) => {
+  //   if (id == mockIdError) throw new NotFoundException();
+  //   return this;
+  // });
+  // static exec = jest.fn().mockReturnValue(mockUser);
+  // static select = jest.fn().mockReturnThis();
+  // static findOne = jest.fn().mockImplementation((id: string) => {
+  //   if (id == mockIdError) throw new NotFoundException();
+  //   return this;
+  // });
+}
+
+
+describe('UsersService', () => {
+  let controller: UsersController;
+  let service: UsersService;
+
+  const UsererviceProvider = {
+    provide: UsersService,
+    useValue: MockedUsersModel,
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsererviceProvider],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should find all user data', async () => {
+    const expectedOutput = await controller.findAll();
+    expect(service.findAll).toHaveBeenCalledTimes(1);
+    expect(expectedOutput).toContain(mockUser);
+  });
+
+  it('should find user data by id', async () => {
+    const expectedOutput = await controller.findOne(mockId);
+    expect(service.findOne).toHaveBeenCalledTimes(1);
+    expect(service.findOne).toHaveBeenCalledWith(mockId);
+    expect(expectedOutput).toEqual(mockUser);
+  });
+
+  it('should create user data', async () => {
+    const expectedOutput = await controller.create(mockUser);
+    expect(service.create).toHaveBeenCalledTimes(1);
+    expect(service.create).toHaveBeenCalledWith(mockUser);
+    expect(expectedOutput).toEqual(mockUser);
+  });
+
+  it('should update user data by id and payload', async () => {
+    const expectedOutput = await controller.update(mockId, mockPartialUser);
+    expect(service.update).toHaveBeenCalledTimes(1);
+    expect(service.update).toHaveBeenCalledWith(mockId, mockPartialUser);
+    expect(expectedOutput).toEqual({ ...mockUser, ...mockPartialUser });
+  });
+
+  it('should delete user data by id', async () => {
+    const expectedOutput = await controller.delete(mockId);
+    expect(service.delete).toHaveBeenCalledTimes(1);
+    expect(service.delete).toHaveBeenCalledWith(mockId);
+    expect(expectedOutput).toEqual(mockUser);
+  });
+});
