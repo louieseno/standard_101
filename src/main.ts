eeba24a0310dad3a8e9e99204e781d101cdb120c
Louie Seno <senolouiejohn.dev@gmail.com>
2025-01-01 02:35:34 +0800

    Add API version

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const apiVersion = process.env.API_VERSION;
 
-  app.setGlobalPrefix('api/v1')
+  app.setGlobalPrefix(`api/${apiVersion}`)
 
   const config = new DocumentBuilder()
     .setTitle('Users API')
     .setDescription('Basic REST API for Users.')
-    .setVersion('1.0')
+    .setVersion(apiVersion)
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api/v1', app, documentFactory);
+  SwaggerModule.setup(`api/${apiVersion}`, app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
